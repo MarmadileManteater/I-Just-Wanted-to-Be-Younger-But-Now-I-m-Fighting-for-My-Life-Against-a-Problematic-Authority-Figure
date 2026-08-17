3216c0102b688673f41d4a6ed43366568bb0b7a9
Emma Manteater <marmadilemanteater@proton.me>
2026-08-16 21:28:13 -0400

    Add mor animations for boss 1

--- project/scenes/boss_1/boss_1.gd
+++ project/scenes/boss_1/boss_1.gd
@@ -5,6 +5,12 @@ extends DefaultScene
 func _ready() -> void:
 	super()
 	willow.bounce(1.5)
+	var timer = Timer.new()
+	add_child(timer)
+	timer.start(5)
+	timer.connect("timeout", func ():
+		find_child("SlamAnimationPlayer").play("LeftArmSlam")
+	)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
